94d89ed5da0d662c97e3f66645b99e9b187a0472
Muhammad Shahnawaz <100338460+MuhammadShahnawaz-MS@users.noreply.github.com>
2023-04-20 16:55:22 +0530

    Add files via upload
    Developed By Amol Pawar

---
+++ Netflix.js
@@ -0,0 +1,11 @@
+let english = document.querySelector('.E');
+let hindi = document.querySelector('.h');
+
+let lang = document.querySelector('.l1');
+
+
+english.addEventListener('click',()=>{
+    document.getElementsByClassName('E').innerHTML=lang;
+})
+
+
